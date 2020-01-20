5d2762a122d867d6421a1c7f6b08b7b658fbedb5
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-20 16:46:51 +0800

    add room database support

--- app/src/main/java/com/example/dogs/model/DogDao.kt
+++ app/src/main/java/com/example/dogs/model/DogDao.kt
@@ -1,4 +1,38 @@
 package com.example.dogs.model
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+/**
+ * To access the database.
+ */
+@Dao
 interface DogDao {
+
+    /**
+     * Inserts data to database and returns a list.
+     *
+     * Suspend to allow running in background thread,
+     * thus not interrupting main thread
+     *
+     */
+    @Insert
+    suspend fun insertAll(vararg dogs: DogBreed): List<Long>
+
+    /**
+     *
+     */
+    @Query("Select * FROM dogbreed")
+    suspend fun getAllDogs():List<DogBreed>
+
+    @Query("SELECT * FROM dogbreed WHERE uuid = :dogId")
+    suspend fun getDog(dogId: Int): DogBreed
+
+    /**
+     * delete every entity in db dogbreed
+     */
+    @Query("DELETE FROM dogbreed")
+    suspend fun deleteAllDogs()
+
 }
--- app/src/main/java/com/example/dogs/model/DogsApi.kt
+++ app/src/main/java/com/example/dogs/model/DogsApi.kt
@@ -4,6 +4,6 @@ import io.reactivex.Single
 import retrofit2.http.GET
 
 interface DogsApi {
-    @GET("DevTides/DogsApi/master/dogs.json")
+    @GET("/DevTides/DogsApi/master/dogs.json")
     fun getDogs(): Single<List<DogBreed>>
 }
