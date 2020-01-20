58622a60bfe031cfc2083ce289c3ff266a48333f
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-20 16:47:40 +0800

    load image

--- app/src/main/java/com/example/dogs/util/Util.kt
+++ app/src/main/java/com/example/dogs/util/Util.kt
@@ -1,2 +1,30 @@
 package com.example.dogs.util
 
+import android.app.DownloadManager
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.dogs.R
+
+fun getProgressDrawable(context: Context) : CircularProgressDrawable {
+
+    // give a spinner image while loading other images
+    return CircularProgressDrawable(context).apply{
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_dog_icon) //give a default image if fail to load
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
