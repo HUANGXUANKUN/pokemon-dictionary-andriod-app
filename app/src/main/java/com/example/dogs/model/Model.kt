160f9e52b535e347d5477f38333433d183522cae
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-20 16:47:20 +0800

    add room entity annotation to model

--- app/src/main/java/com/example/dogs/model/Model.kt
+++ app/src/main/java/com/example/dogs/model/Model.kt
@@ -1,28 +1,42 @@
 package com.example.dogs.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+// data class doesn't necessary need a body
+@Entity
 data class DogBreed(
 
-    // serialized with the name in json
-    @SerializedName("id")
+    @ColumnInfo(name = "breed_id") // column name stored in db
+    @SerializedName("id") // serialized with the name in json
     val breedId: String?,
 
+    @ColumnInfo(name = "dog_name")
     @SerializedName("name")
-    val lifeSpan: String?,
-
-    @SerializedName("life_span")
-    val breedGroup: String?,
-
-    @SerializedName("breed_group")
     val dogBreed: String?,
 
+    @ColumnInfo(name = "breed_group")
+    @SerializedName("breed_group")
+    val breedGroup: String?,
+
+    @ColumnInfo(name = "life_span")
+    @SerializedName("life_span")
+    val lifeSpan: String?,
+
+    @ColumnInfo(name = "bred_for")
     @SerializedName("bred_for")
     val bredFor: String?,
 
     @SerializedName("temperament")
     val temperament: String?,
 
+    @ColumnInfo(name = "dog_url")
     @SerializedName("url")
     val imageUrl: String?
-)
+) {
+    // define primary key in the body
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
